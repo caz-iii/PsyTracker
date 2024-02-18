ccef76b010fb130dfc1653439064011667ee1ab2
caz-iii <caz@codingwithcarl.com>
2024-02-18 12:59:44 -0500

    Update MainWindow.xaml.cs

--- PsychonautsItemTracker/MainWindow.xaml.cs
+++ PsychonautsItemTracker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PsychonautsItemTracker
 {
@@ -16,9 +18,12 @@ namespace PsychonautsItemTracker
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
-            InitializeComponent();
+           
         }
+
+
     }
 }
