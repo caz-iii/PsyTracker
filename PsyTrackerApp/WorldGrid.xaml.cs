2b4fdb5127f192e35da457bbf27596c11028b982
caz-iii <caz@codingwithcarl.com>
2024-05-06 14:12:50 -0400

    Update WorldGrid.xaml.cs

--- PsyTrackerApp/WorldGrid.xaml.cs
+++ PsyTrackerApp/WorldGrid.xaml.cs
@@ -50,52 +50,6 @@ namespace PsyTrackerApp
 
             Item button = e.Data.GetData(typeof(Item)) as Item;
 
-           // bool isreport = false;
-
-            // item is a report
-            //if (data.hintsLoaded && (int)button.GetValue(Grid.RowProperty) == 0)
-            //{
-            //    int index = (int)button.GetValue(Grid.ColumnProperty);
-
-                // out of report attempts
-                //if (data.reportAttempts[index] == 0)
-                //    return;
-
-                // check for correct report location
-                //if (data.reportLocations[index].Replace(" ", "") == grid.Name.Substring(0, grid.Name.Length - 4))
-                //{
-                //    // hint text and resetting fail icons
-                //    window.SetHintText(data.reportInformation[index].Item1 + " has " + data.reportInformation[index].Item2 + " important checks.");
-                //    data.ReportAttemptVisual[index].SetResourceReference(ContentControl.ContentProperty, "Fail0");
-                //    data.reportAttempts[index] = 3;
-                //    isreport = true;
-
-                //    // auto update world immportant check number
-                //    for (int i = 0; i < data.Hints.Count; ++i)
-                //    {
-                //        if (data.Worlds[i].Name == data.reportInformation[index].Item1.Replace(" ", ""))
-                //            window.SetReportValue(data.Hints[i], data.reportInformation[index].Item2 + 1);
-                //    }
-                //}
-                //else
-                //{
-                //    // update fail icons when location is report location is wrong
-                //    data.reportAttempts[index]--;
-                //    if (data.reportAttempts[index] == 0)
-                //        data.ReportAttemptVisual[index].SetResourceReference(ContentControl.ContentProperty, "Fail3");
-                //    else if (data.reportAttempts[index] == 1)
-                //        data.ReportAttemptVisual[index].SetResourceReference(ContentControl.ContentProperty, "Fail2");
-                //    else if (data.reportAttempts[index] == 2)
-                //        data.ReportAttemptVisual[index].SetResourceReference(ContentControl.ContentProperty, "Fail1");
-
-                //    return;
-                //}
-            //}
-
-            // remove hint text when tracking a non-report
-            //if (isreport == false)
-            //    window.SetHintText("");
-
             // move item to world
             window.ItemPoolGrid.Children.Remove(button);
             grid.Children.Add(button);
